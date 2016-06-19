c5475c8945228deef1d208a390011b27ad7ff020
Alex <alex@krien.name>
2016-06-19 10:06:56 +0200

    Added CamPos and CamRot to Debug Stats

--- src/main/java/de/krien/games/madness/view/hud/stats/Stats.java
+++ src/main/java/de/krien/games/madness/view/hud/stats/Stats.java
@@ -1,6 +1,8 @@
 package de.krien.games.madness.view.hud.stats;
 
+import de.krien.games.madness.view.camera.Camera;
 import de.krien.games.madness.view.hud.GameObject2D;
+import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.TrueTypeFont;
 
@@ -23,11 +25,30 @@ public class Stats implements GameObject2D {
 
     public void draw() {
         drawFps();
+        drawCameraPosition();
+        drawCameraRotation();
     }
 
     private void drawFps() {
         updateFps();
         typeFont.drawString(5, 5, "FPS: " + getFramesPerSecond(), Color.black);
+    }
+
+    private void drawCameraPosition() {
+        Vector3f position = Camera.INSTANCE.getPosition();
+        float positionX = Math.round(position.getX()* 100f) / 100f;
+        float positionY = Math.round(position.getY()* 100f) / 100f;
+        float positionZ = Math.round(position.getZ()* 100f) / 100f;
+        typeFont.drawString(5, 25, "Camera Position: " + positionX + "/" + positionY + "/" + positionZ, Color.black);
+    }
+
+
+    private void drawCameraRotation() {
+        Vector3f rotation = Camera.INSTANCE.getRotation();
+        float rotationX = Math.round(rotation.getX()* 100f) / 100f;
+        float rotationY = Math.round(rotation.getY()* 100f) / 100f;
+        float rotationZ = Math.round(rotation.getZ()* 100f) / 100f;
+        typeFont.drawString(5, 45, "Camera Position: " + rotationX + "/" + rotationY + "/" + rotationZ, Color.black);
     }
 
     private void updateFps() {
